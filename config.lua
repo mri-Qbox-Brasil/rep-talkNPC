5a3c552f7c1e50c8f86bda466bea5324865c7647
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-16 13:39:57 -0300

    Fix: update cdn adobe link fonts

--- config.lua
+++ config.lua
@@ -6,6 +6,6 @@ Config.Color = {
     -- which could be found at https://mantine.dev/theming/colors/#default-colors
     -- using the syntax: "{color}.{shade}"
     -- also supports colors in Hex
-    primaryColor = "pink.7",   -- Button hover effect
-    secondaryColor = "#2E67E7" -- Npc default tag color if you don't set them while creating
+    primaryColor = "green",   -- Button hover effect
+    secondaryColor = "green" -- Npc default tag color if you don't set them while creating
 }
